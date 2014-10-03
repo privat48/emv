4a67063dacdd9a92b9479ec9df065b503f963202
Maxim Sokhatsky <maxim@synrc.com>
2014-10-03 21:06:36 +0300

    init

---
+++ Makefile
@@ -0,0 +1,24 @@
+# leave these lines alone
+.SUFFIXES: .erl .beam
+QCLIB = deps/eqc/ebin/
+ERL = erl -boot start_clean 
+MODS = credit_card credit_card_test credit_card_server credit_card_eqc
+
+.erl.beam:
+	erlc -W -pa $(QCLIB) -o ./ebin $<
+
+compile: ${MODS:%=./src/%.beam}
+	
+
+clean:	
+	rm -rf *.beam erl_crash.dump
+	rm -rf ebin/*.beam
+
+test: compile
+	./test.escript ebin
+	
+quickcheck: compile
+	erl -pa ebin/ -pa $(QCLIB) -eval 'eqc:module(credit_card_eqc).'
+	
+server: compile
+	erl -pz ebin/ -eval 'credit_card_server:start().'
---
+++ src/credit_card.erl
@@ -0,0 +1,53 @@
+-module(credit_card).
+-compile(export_all).
+
+%% Returns a boolean value of whether the card is valid or not.
+is_valid(N) ->
+  N2 = lists:reverse(list_to_integers(N)),
+  sum_digits(N2) rem 10 =:= 0.
+
+%% Returns an atom describing the type of the card
+%% Possible outputs are: amex, visa, mastercard, discover, unknown
+type(N) ->
+  type_(N, string:len(N)). 
+
+sum_digits(N) ->
+  sum_digits(N,0).
+  
+sum_digits([],Acc) ->
+  Acc;
+
+sum_digits([Odd], Acc) ->
+  Acc + Odd;
+  
+sum_digits(List, Acc) ->
+  [Odd|Tail] = List,
+  [Even|Remainder] = Tail,
+  sum_digits(Remainder, Acc + digital_root(Even * 2) + Odd).
+
+digital_root(N) ->
+   1 + (N-1) rem 9.
+   
+list_to_integers(N) ->
+  lists:map(fun(E) -> list_to_integer([E]) end, N).
+
+
+%% 34* or 37*
+type_([$3|[Second|_]], Length) when Length =:= 15, Second =:= $4; Second =:= $7 ->
+  amex;
+
+%% 4*
+type_([$4|_],Length) when Length =:= 13; Length =:= 16 ->
+  visa;
+
+%% 51-55*
+type_([$5|[Second|_]], Length) when Length =:= 16, Second >= $1, Second =< $5 ->
+  mastercard;
+
+%% 6011*
+type_([$6|[$0|[$1|[$1|_]]]], Length) when Length =:= 16 ->
+  discover; 
+
+type_(_, _) ->
+  unknown.
+  
